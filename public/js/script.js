39e15be2370030674c7734cef75eea648b90e2ed
Darko Seleši <hallo@w3scouts.com>
2023-07-27 19:30:56 +0200

    Refactored JS

--- public/js/script.js
+++ public/js/script.js
@@ -2,17 +2,30 @@
 
 (function () {
     document.addEventListener("DOMContentLoaded", function() {
-        let reviews= document.querySelectorAll('.review');
-        reviews.forEach(function(r) {
-            let t = r.querySelector('.text'),
-                readmore = r.querySelector('.readmore');
-            if(t.innerText.length >= 200) {
-                readmore.style.display = 'block';
-                readmore.addEventListener('click', function(e) {
-                    e.preventDefault();
-                    this.parentNode.classList.toggle('open');
-                });
+
+        function initToggles() {
+            // console.log("initToggles");
+
+            function toggleText(el) {
+                console.log("toogle", el);
+                el.parentNode.classList.contains('open') ? el.innerText = el.getAttribute('data-readless') : el.innerText = el.getAttribute('data-readmore');
             }
-        });
+
+            let reviews= document.querySelectorAll('.review');
+            reviews.forEach(function(review) {
+                let t = review.querySelector('.text'),
+                    r = review.querySelector('.readmore');
+                if(t.innerText.length >= 200) {
+                    review.classList.contains('open') ? r.innerText = r.getAttribute('data-readless') : r.innerText = r.getAttribute('data-readmore')
+                    r.style.display = 'block';
+                    r.addEventListener('click', function(e) {
+                        e.preventDefault();
+                        this.parentNode.classList.toggle('open');
+                        toggleText(this);
+                    });
+                }
+            });
+        }
+        initToggles();
     });
 })();
